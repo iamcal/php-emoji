323cc5bf9947c48351c06a467d35e67f5e8a2865
dzg <dzg@linux-kmbw.(none)>
2011-03-26 12:34:06 +0800

    update parse.php and emoji.php

--- data_src/parse.php
+++ data_src/parse.php
@@ -1,272 +1,369 @@
-<?
-	header('Content-type: text/plain; charset=UTF-8');
+<?php
 
-	$data = file_get_contents('source.htm');
+//require php 5.3.0+
 
-	$items = array();
+$doc = new DOMDocument();
+@$doc->loadHTML(str_ireplace(
+					array('<br>', '<br/>', '<br />'),
+					"\n",
+					file_get_contents($argv[1])));
 
-	preg_match_all('!<tr(.*?)</tr>!', $data, $mtr);
+$trs = get_elts_by_tag($doc, 'tr');
 
-	foreach ($mtr[1] as $tr){
+$trs = filter_els($trs, array(
+		function ($item){
+			return cond_attr_not($item, 'class', 'not_in_proposal');
+		} ,
+		function ($item){
+			return cond_attr_match($item, 'id', '/^e-\w{3}$/');
+		} ,
+		function ($item){
+			return 7 === count(get_elts_by_tag($item, 'td')) ;
+		} ,
+	));
 
+fprintf(STDERR, "trs count:" . count($trs)."\n");
 
-		#
-		# is the a category row?
-		#
+$mapping = array();
 
-		if (preg_match('!class=\'category\'!', $tr)){
-			if (preg_match('!.>(.*?)<!', $tr, $m)){
+foreach($trs as $tr) {
+	$map = array();
 
-				$cat = $m[1];
-				continue;
-			}
-		}
+	$tds = get_elts_by_tag($tr, 'td');
 
+	$map['mapid'] = parse_mapid($tds[0]);
+	$map['unicode'] = parse_unicode($tds[1]);
+	$map['char_name'] = parse_char_name($tds[2]);
+	$map['docomo'] = parse_mobile($tds[3]);
+	$map['au'] = parse_mobile($tds[4]);
+	$map['softbank'] = parse_mobile($tds[5]);
+	$map['google'] = parse_google($tds[6]);
 
-		#
-		# is the a subcategory row?
-		#
+	$mapping[] = $map;
+}
 
-		if (preg_match('!class=\'subcategory\'!', $tr)){
-			if (preg_match('!.>(.*?) \(.*\)<!', $tr, $m)){
+fprintf(STDERR, "mapping count:" . count($mapping)."\n");
 
-				$subcat = $m[1];
-				continue;
-			}
-		}
+//filter invalid mapping
+fprintf(STDERR, "filter only_kaomoji ; like e-554 -> [A] -> [A] -> [A] -> [A]\n");
+$mapping = filter_only_kaomoji($mapping);
+fprintf(STDERR, "mapping count:" . count($mapping)."\n");
 
+fprintf(STDERR, "filter chars-group ; like #44+#139\n");
+$mapping = filter_chars_group($mapping);
+fprintf(STDERR, "mapping count:" . count($mapping)."\n");
 
-		#
-		# is this a character row?
-		#
+$emoji_maps['kaomoji'] = get_all_kaomoji($mapping);
 
-		if (preg_match('!id=(e-[0-9A-F]+)!', $tr, $m)){
+#fprintf(STDERR, "fix Geta Mark ()  '〓' (U+3013)\n");
+#$mapping = fix_geta_mark($mapping);
 
-			$code = $m[1];
+//export mapping array
+$emoji_maps["unified_to_docomo"] = make_mapping($mapping, 'unicode', 'docomo');
+$emoji_maps["unified_to_kddi"] = make_mapping($mapping, 'unicode', 'au');
+$emoji_maps["unified_to_softbank"] = make_mapping($mapping, 'unicode', 'softbank');
+$emoji_maps["unified_to_google"] = make_mapping($mapping, 'unicode', 'google');
+$emoji_maps["docomo_to_unified"] = make_mapping_flip($mapping, 'unicode', 'docomo');
+$emoji_maps["kddi_to_unified"] = make_mapping_flip($mapping, 'unicode', 'au');
+$emoji_maps["softbank_to_unified"] = make_mapping_flip($mapping, 'unicode', 'softbank');
+$emoji_maps["google_to_unified"] = make_mapping_flip($mapping, 'unicode', 'google');
 
-			preg_match_all('!<td(.*?)</td>!', $tr, $cells);
-			$cells = $cells[1];
-			$num = count($cells);
-
-			if ($num != 7){
-				echo "# weird - found $num cells instead of 7...\n";
-				continue;
-			}
-
-
-			#
-			# get unified code point
-			#
-
-			$unified = 0;
-			if (preg_match('!U\+([0-9A-F]{4,6})!', $cells[1], $m)) $unified = hexdec($m[1]);
-
-
-			#
-			# get name
-			#
-
-			$name = 'unknown';
-			if (preg_match('!>(.*?)(<|$)!', $cells[2], $m)) $name = $m[1];
-
-
-			#
-			# get different formats
-			#
-
-			$docomo		= parse_cell($cells[3]);
-			$kddi		= parse_cell($cells[4]);
-			$softbank	= parse_cell($cells[5]);
+echo '<'.'?'.'php'."\n";
+echo '$GLOBALS["emoji_maps"] = ';
+echo	my_var_export($emoji_maps);
+echo ";\n";
 
 
-			#
-			# get google
-			#
+//-----  functions ------------------
 
-			$google = 0;
-			if (preg_match('!U\+([0-9A-F]{4,6})!', $cells[6], $m)) $google = hexdec($m[1]);
+function filter_els($elts, $conds) {
+	$result = array();
+	foreach($elts as $elt) {
+		$bl = true;
 
-			if ($unified > 0x80){
-
-				$items[] = array(
-					'code'		=> $code,
-					'unified'	=> $unified,
-					'name'		=> $name,
-					'docomo'	=> $docomo,
-					'kddi'		=> $kddi,
-					'softbank'	=> $softbank,
-					'google'	=> $google,
-				);
-			}
-
-			continue;
-		}
-
-
-		#
-		# something unexpected
-		#
-
-		echo "# unknown row: ".HtmlSpecialChars($tr)."\n";
-	}
-
-
-	#
-	# build the mapping arrays
-	#
-
-	$maps = array();
-
-	foreach ($items as $row){
-
-		$maps[names][$row[unified]] = $row[name];
-		$unified_bytes = emoji_utf8_bytes($row[unified]);
-
-		foreach (array('docomo', 'kddi', 'softbank') as $type){
-
-			if ($row[$type][uni]){
-
-				$type_bytes = emoji_utf8_bytes($row[$type][uni]);
-
-				$maps["unified_to_{$type}"][$unified_bytes] = $type_bytes;
-
-				if ($row[$type][uni] > 0x80 && !$row[$type][oneway]){
-					$maps["{$type}_to_unified"][$type_bytes] = $unified_bytes;
+		foreach($conds as $func) 
+				if(! $func($elt) ) {
+					$bl = false;
+					break;
 				}
 
-			}else if ($row[$type][fallback]){
-
-				$maps["unified_to_{$type}"][$unified_bytes] = $row[$type][fallback];
-			}else{
-				$maps["unified_to_{$type}"][$unified_bytes] = '?';
-			}
+		if($bl) {
+			$result[] = $elt;
 		}
-
-		$google_bytes = emoji_utf8_bytes($row[google]);
-
-		$maps["unified_to_google"][$unified_bytes] = $google_bytes;
-
-		if ($row[google] > 0x80){
-			$maps["google_to_unified"][$google_bytes] = $unified_bytes;
-		}
-
-
-		$hex = sprintf('%x', $row[unified]);
-
-		$maps["unified_to_html"][$unified_bytes] = "<span class=\"emoji emoji$hex\"></span>";
 	}
+	return $result;
+}
 
+function cond_attr_not($item, $name, $value) {
+	$attr = $item->getAttribute($name);
+	return $attr != $value;
+}
 
-	#
-	# output
-	# we could just use var_dump, but we get 'better' output this way
-	#
+function cond_attr_match($item, $name, $regex) {
+	$attr = $item->getAttribute($name);
+	return preg_match($regex, $attr) ;
+}
 
-	echo "<"."?php\n";
-
-	echo "\n";
-	echo "\t#\n";
-	echo "\t# WARNING:\n";
-	echo "\t# This code is auto-generated. Do not modify it manually.\n";
-	echo "\t#\n";
-	echo "\n";
-
-	echo "\t\$GLOBALS['emoji_maps'] = array(\n";
-
-	echo "\t\t'names' => array(\n";
-
-	foreach ($maps[names] as $k => $v){
-
-		$name_enc = escape_string($v);
-		echo "\t\t\t$k => $name_enc,\n";
+function node_list_to_arr($nodelist) {
+	$result = array();
+	$len = $nodelist->length;
+	for($i=0; $i<$len; $i++) {
+		$result[] = $nodelist->item($i);
 	}
+	return $result;
+}
 
-	echo "\t\t),\n";
+function get_elts_by_tag($root, $tagname) {
+	return node_list_to_arr(
+		$root->getElementsByTagName($tagname));
+}
 
-	foreach ($maps as $k => $v){
+function parse_mapid($elt) {
+	// like <a href="#e-19E">e-19E</a>
+	$links = get_elts_by_tag($elt, 'a');
+	if(empty($links)) return null;
+	return $links[0]->getAttribute("href");
+}
 
-		if ($k == 'names') continue;
+function parse_unicode($elt) {
+	//like U+1F469
+	return get_unicode_char($elt);
+}
 
-		echo "\t\t'$k' => array(\n";
+function parse_char_name($elt) {
+	$lines = array_filter(array_map('trim', explode("\n", $elt->textContent)));
 
-		$count = 0;
-		echo "\t\t\t";
-		foreach ($v as $k2 => $v2){
-			$count++;
-			if ($count % 10 == 0) echo "\n\t\t\t";
-			echo format_string($k2).'=>'.format_string($v2).', ';
-		}
-		echo "\n";
+	if(count($lines[0])>0) {
+		$result['title'] = $lines[0];
+		unset($lines[0]);
+		$result['desc'] = join("\n", $lines);
 
-		echo "\t\t),\n";
+		return $result;
 	}
+	return null;
+}
 
-	echo "\t);\n";
-
-
-
-	function parse_cell($cell){
-
-		$out = array(
-			'uni'		=> 0,
-			'sjis'		=> 0,
-			'jis'		=> 0,
-			'fallback'	=> '',
-			'oneway'	=> 0,
+function parse_mobile($elt) {
+	if(!has_image($elt)) {
+		return array(
+			'kaomoji' => trim($elt->textContent)
 		);
+	}
+	else {
+		return array(
+			'number' => get_number_char($elt) ,
+			'unicode'=> get_unicode_char($elt),
+			'sjis' => get_sjis_char($elt),
+			'jis' => get_jis_char($elt),
+			);
+	}
+}
 
-		if (preg_match('!U\+([0-9A-F]{4,6})!', $cell, $m)) $out[uni] = hexdec($m[1]);
-		if (preg_match('!SJIS\-([0-9A-F]{4,6})!', $cell, $m)) $out[sjis] = hexdec($m[1]);
-		if (preg_match('!JIS\-([0-9A-F]{4,6})!', $cell, $m)) $out[jis] = hexdec($m[1]);
-		if (preg_match('!class=\'text_fallback\'>(.*?)(<|$)!', $cell, $m)) $out[fallback] = $m[1];
-		if (preg_match('!class=\'fallback\'>(.*?)(<|$)!', $cell, $m)) $out[oneway] = 1;
+function parse_google($elt) {
+	return array(
+		'unicode' => get_unicode_char($elt)
+	);
+}
 
-		return $out;
+function has_image($elt) {
+	return count(get_elts_by_tag($elt, 'img')) > 0;
+}
+
+function get_unicode_char($elt) {
+	$r = preg_match('/U\+(\w{4,5})/u', $elt->textContent, $match);
+	return $r ? $match[1] : null;
+}
+
+function get_sjis_char($elt) {
+	$r = preg_match('/\bSJIS-(\w{4})/u', $elt->textContent, $match);
+	return $r ? $match[1] : null;
+}
+
+function get_jis_char($elt) {
+	$r = preg_match('/\bJIS-(\w{4})/u', $elt->textContent, $match);
+	return $r ? $match[1] : null;
+}
+
+function get_number_char($elt) {
+	$r = preg_match('/\#([a-zA-Z0-9\.+-]{1,})/', $elt->textContent, $match);
+	return $r ? $match[1] : null;
+}
+
+
+
+function filter_only_kaomoji($mapping) {
+	$result = array();
+	foreach($mapping as $map) {
+
+		if(isset($map['docomo']['kaomoji'])
+			&& isset($map['au']['kaomoji'])
+			&& isset($map['softbank']['kaomoji'])) 
+		{
+			continue;
+		}
+		else {
+			$result[] = $map;
+		}
 	}
 
+	return $result;
+}
 
-	function escape_bytes($cp){
-		return escape_string(emoji_utf8_bytes($cp));
+
+function filter_chars_group($mapping) {
+	$result = array();
+	foreach($mapping as $map) {
+
+		if( @preg_match('/\+$/', $map['docomo']['number']) 
+			|| @preg_match('/\+$/', $map['au']['number']) 
+			|| @preg_match('/\+$/', $map['softbank']['number']) 
+		){
+			continue;
+		}
+		else {
+			$result[] = $map;
+		}
 	}
 
-	function emoji_utf8_bytes($cp){
+	return $result;
+}
 
-		if ($cp > 0x10000){
-			# 4 bytes
-			return	chr(0xF0 | (($cp & 0x1C0000) >> 18)).
-				chr(0x80 | (($cp & 0x3F000) >> 12)).
-				chr(0x80 | (($cp & 0xFC0) >> 6)).
-				chr(0x80 | ($cp & 0x3F));
-		}else if ($cp > 0x800){
-			# 3 bytes
-			return	chr(0xE0 | (($cp & 0xF000) >> 12)).
-				chr(0x80 | (($cp & 0xFC0) >> 6)).
-				chr(0x80 | ($cp & 0x3F));
-		}else if ($cp > 0x80){
-			# 2 bytes
-			return	chr(0xC0 | (($cp & 0x7C0) >> 6)).
-				chr(0x80 | ($cp & 0x3F));
+function fix_geta_mark($mapping) {
+	$result = array();
+
+	foreach($mapping as $map) {
+
+		if(isset($map['docomo']['kaomoji']) 
+			&& $map['docomo']['kaomoji'] == '〓') {
+			$map['docomo']['kaomoji'] = '';
+		}
+
+		if(isset($map['au']['kaomoji']) 
+			&& $map['au']['kaomoji'] == '〓') {
+			$map['au']['kaomoji'] = '';
+		}
+
+		if(isset($map['softbank']['kaomoji']) 
+			&& $map['softbank']['kaomoji'] == '〓') {
+			$map['softbank']['kaomoji'] = '';
+		}
+
+		$result[] = $map;
+	}
+
+	return $result;
+}
+
+function get_all_kaomoji($mapping) {
+	$arr = array();
+
+	foreach($mapping as $map) {
+		if(isset($map['docomo']['kaomoji']) ) {
+			$arr[ $map['docomo']['kaomoji'] ] = '1';
+		}
+
+		if(isset($map['au']['kaomoji']) ) {
+			$arr[ $map['au']['kaomoji'] ] = '1';
+		}
+
+		if(isset($map['softbank']['kaomoji']) ) {
+			$arr[ $map['softbank']['kaomoji'] ] = '1';
+		}
+	}
+
+	return array_keys($arr);
+}
+
+function make_mapping($mapping, $unicode_key, $dest) {
+	
+	$result = array();
+
+	foreach($mapping as $map) {
+		$src_code = intval($map[$unicode_key] , 16);
+		$src_char = int2utf8($src_code);
+		//debug end
+
+		//debug
+//		$src_char = $map[$unicode_key];
+
+		if(!empty( $map[$dest]['unicode'] )) {
+			$dest_code = intval($map[$dest]['unicode'], 16);
+			$dest_char = int2utf8($dest_code);
+
+			//debug
+//			$dest_char = $map[$dest]['unicode'];
+		}
+		else {
+			$dest_char = $map[$dest]['kaomoji'];
+		}
+
+		$result[$src_char] = $dest_char ;
+	}
+
+	return $result;
+}
+
+function make_mapping_flip($mapping, $unicode_key, $src) {
+	$result = make_mapping($mapping, $unicode_key, $src);
+	$result = array_flip($result);
+	unset($result[""]);
+	return $result;
+}
+
+function int2utf8($code) {
+	$iByte = 0;
+	$i = 0;
+	$result = "";
+
+	while($code > 0x7f)	{
+		$iByte = $code % 0x40;
+		$code = ($code - $iByte)/0x40;
+		$result =  chr($iByte|0x80) . $result;
+		$i++;
+	}
+
+	$prefix_arr = array(0x0, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc);
+
+	if($i > count($prefix_arr))	{
+		$i = 5;
+	}
+
+	$result= chr($code|$prefix_arr[$i]) . $result;
+	return $result;
+}
+
+
+function format_string($s){
+	$out = ''; 
+	for ($i=0; $i<strlen($s); $i++){
+		$c = ord(substr($s,$i,1));
+		if ($c >= 0x20 && $c < 0x80 && !in_array($c, array(34, 39, 92))){
+			$out .= chr($c);
 		}else{
-			# 1 byte
-			return chr($cp);
+			$out .= sprintf('\\x%02x', $c);
+		}   
+	}   
+	return '"'.$out.'"';
+}   
+
+function my_var_export($arr) {
+	$tab = "\t\t";
+	$str = "array(\n";
+	foreach($arr as $name => $value) {
+		$str .= $tab ;
+		$str .= format_string($name). "\t=>\t" ;
+		if(is_array($value)) {
+			$exp = my_var_export($value) ;
+			$str .= preg_replace('/^/', $tab, $exp) ."\t,\n";
+		}
+		elseif(is_object($value)) {
+			$exp = var_export($value, true) . "\t,\n";
+			$str .= preg_replace('/^/', $tab, $exp);
+		}
+		else {
+			$str .= format_string($value). "\t,\n" ;
 		}
 	}
-
-
-	function escape_string($s){
-		return "'".AddSlashes($s)."'";
-	}
-
-	function format_string($s){
-		$out = '';
-		for ($i=0; $i<strlen($s); $i++){
-			$c = ord(substr($s,$i,1));
-			if ($c >= 0x20 && $c < 0x80 && !in_array($c, array(34, 39, 92))){
-				$out .= chr($c);
-			}else{
-				$out .= sprintf('\\x%02x', $c);
-			}
-		}
-		return '"'.$out.'"';
-	}
-?>
+	$str .= "\t)\n";
+	return $str;
+}
