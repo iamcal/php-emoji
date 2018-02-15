e65468ffd90d9814ee55840dd1863838727b4765
Maher <mh@km-it.de>
2018-02-15 15:54:33 +0100

    * built using recent emoji-data with new emojis from 2017, e.g. Face With Open Mouth Vomiting * support for skin tones * support for compound emojis made of serveral single emojis, e.g. Family: Man, Woman, Girl, Boy

--- test/test.php
+++ test/test.php
@@ -19,13 +19,13 @@
 	# this test uses unified U+2649 - TAURUS
 	#
 
-	$test_unified	= "Hello ".utf8_bytes(0x2649);
-	$test_iphone	= "Hello ".utf8_bytes(0xE240);
-	$test_docomo	= "Hello ".utf8_bytes(0xE647);
-	$test_kddi	= "Hello ".utf8_bytes(0xE490);
-	$test_google	= "Hello ".utf8_bytes(0xFE02C);
-
-	$test_html	= "Hello ".test_html(2649);
+	$test_unified	= "TAURUS ".utf8_bytes(0x2649);
+	$test_iphone	= "TAURUS ".utf8_bytes(0xE240);
+	$test_docomo	= "TAURUS ".utf8_bytes(0xE647);
+	$test_kddi	= "TAURUS ".utf8_bytes(0xE490);
+	$test_google	= "TAURUS ".utf8_bytes(0xFE02C);
+	
+	$test_html	= "TAURUS ".test_html(2649);
 
 	is(emoji_docomo_to_unified($test_docomo),	$test_unified, "DoCoMo -> Unified");
 	is(emoji_kddi_to_unified($test_kddi),		$test_unified, "KDDI -> Unified");
@@ -53,29 +53,21 @@
 	# expected
 	#
 
-	$test_unified	= "Hello ".utf8_bytes(0x36).utf8_bytes(0x20E3);
-	$test_iphone	= "Hello ".utf8_bytes(0xE221);
-	$test_docomo	= "Hello ".utf8_bytes(0xE6E7);
-	$test_kddi	= "Hello ".utf8_bytes(0xE527);
-	$test_google	= "Hello ".utf8_bytes(0xFE833);
-
-	$test_html	= "Hello <span class=\"emoji-outer emoji-sizer\"><span class=\"emoji-inner emoji3620e3\"></span></span>";
-
+	$test_unified	= "KEYCAP 6 ".utf8_bytes(0x36).utf8_bytes(0xFE0F).utf8_bytes(0x20E3);
+	$test_iphone	= "KEYCAP 6 ".utf8_bytes(0xE221);
+	$test_docomo	= "KEYCAP 6 ".utf8_bytes(0xE6E7);
+	$test_kddi	= "KEYCAP 6 ".utf8_bytes(0xE527);
+	$test_google	= "KEYCAP 6 ".utf8_bytes(0xFE833);
+	
+	$test_html	= "KEYCAP 6 <span class=\"emoji-outer emoji-sizer\"><span class=\"emoji-inner emoji36fe0f20e3\"></span></span>";
 
 	is(emoji_docomo_to_unified($test_docomo),	$test_unified, "DoCoMo -> Unified");
 	is(emoji_kddi_to_unified($test_kddi),		$test_unified, "KDDI -> Unified");
 	is(emoji_softbank_to_unified($test_iphone),	$test_unified, "Softbank -> Unified");
 	is(emoji_google_to_unified($test_google),	$test_unified, "Google -> Unified");
-
+	
 	echo "#------------------\n";
-
-	is(emoji_unified_to_docomo($test_unified),	$test_docomo,	"Unified -> DoCoMo");
-	is(emoji_unified_to_kddi($test_unified),	$test_kddi,	"Unified -> KDDI");
-	is(emoji_unified_to_softbank($test_unified),	$test_iphone,	"Unified -> Softbank");
-	is(emoji_unified_to_google($test_unified),	$test_google,	"Unified -> Google");
-
-	echo "#------------------\n";
-
+	
 	is(emoji_unified_to_html($test_unified),	$test_html,	"Unified -> HTML");
 	is(emoji_html_to_unified($test_html),		$test_unified,	"HTML -> Unified");
 
@@ -86,12 +78,12 @@
 	# names are accessed by the unified codepoint (which makes it tricky for 2-codepoint unicode symbols)
 	#
 
-	is(emoji_get_name(utf8_bytes(0x2600)),	'BLACK SUN WITH RAYS',		"name U+2600");
+	is(emoji_get_name(utf8_bytes(0x2600).utf8_bytes(0xFE0F)),	'BLACK SUN WITH RAYS',		"name U+2600 U+FE0F");
 	is(emoji_get_name(utf8_bytes(0x26EA)),	'CHURCH',			"name U+26EA");
 	is(emoji_get_name(utf8_bytes(0x1F480)),	'SKULL',			"name U+1F480");
 	is(emoji_get_name(utf8_bytes(0x1F450)),	'OPEN HANDS SIGN',		"name U+1F450");
 	is(emoji_get_name(utf8_bytes(0x1F52B)),	'PISTOL',			"name U+1F52B");
-	is(emoji_get_name(utf8_bytes(0x36).utf8_bytes(0x20E3)),	'KEYCAP 6',	"name U+36 U+20E3");
+	is(emoji_get_name(utf8_bytes(0x36).utf8_bytes(0xFE0F). utf8_bytes(0x20E3)),	'KEYCAP 6',	"name U+36 U+FE0F U+20E3");
 
 	echo "#------------------\n";
 
@@ -101,7 +93,7 @@
 	#
 
 	is(emoji_contains_emoji('test '.utf8_bytes(0x2600).' test'),			true, "contains simple emoji");
-	is(emoji_contains_emoji('test '.utf8_bytes(0x36).utf8_bytes(0x20E3).' test'),	true, "contains compound emoji");
+	is(emoji_contains_emoji('test '.utf8_bytes(0x36).utf8_bytes(0xFE0F).utf8_bytes(0x20E3).' test'),	true, "contains compound emoji");
 	is(emoji_contains_emoji('hello world'),						false, "does not contain emoji");
 
 
@@ -112,8 +104,8 @@
 	# deal with modifiers correctly
 	#
 
-	is(emoji_unified_to_html("\xE2\x9D\xA4"),		test_html('2764'),		"no modifier");
-	is(emoji_unified_to_html("\xE2\x9D\xA4\xEF\xB8\x8F"),	test_html('2764'),		"image modifier");
+	is(emoji_unified_to_html("\xE2\x9D\xA4"),	test_html('2764fe0f'),		"no modifier");
+	is(emoji_unified_to_html("\xE2\x9D\xA4\xEF\xB8\x8F"),	test_html('2764fe0f'),		"image modifier");
 	is(emoji_unified_to_html("\xE2\x9D\xA4\xEF\xB8\x8E"),	"\xE2\x9D\xA4\xEF\xB8\x8E",	"text modifier");
 
 
